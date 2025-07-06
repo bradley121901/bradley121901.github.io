24cd76c38e209fd9dcabf74f7434ee369cd7250c
bradley121901 <bradley121901@gmail.com>
2025-07-06 00:11:30 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/bradley121901.github.io/',
-  esbuild: {
-    loader: 'jsx',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
+  build: {
+    outDir: 'dist',
+    assetsInclude: ['**/*.jsx'],
+    rollupOptions: {
+      input: {
+        main: './index.html',
       },
     },
   },
